c5485a80adda7a8f48aa03fe4649338acca8c9c8
hyobin-yang <hyobin3026@naver.com>
2023-08-05 00:02:22 +0900

    comment model

--- articles/views.py
+++ articles/views.py
@@ -3,41 +3,22 @@ import requests
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-# from .models import NYT, Guardian, NYT_Comment, Guardian_Comment
 from .models import NYT, Guardian, Guardian_Comment, NYT_Comment
 from .serializers import NYTSerializer, GuardianSerializer, NYT_CommentSerializer, Guardian_CommentSerializer
 from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 
-# 가디언
-def init_Guardian_db(request):
-    url = f"https://content.guardianapis.com/search?api-key={settings.GUARDIAN_API_KEY}"
-    res = requests.get(url)
-    articles = res.json()['response']['results']
-    for article in articles:
-        try:
-            news_data = Guardian()
-            news_data.title = article['webTitle']
-            news_data.url = article['webUrl']
-            news_data.section = article['sectionName']
-            news_data.save()
-        except:
-            pass
 
 
 
-class GUARDIAN_View(APIView):
-    def get(self, request):
-        articles = Guardian.objects.all()
-        serializer = GuardianSerializer(articles, many=True)
-        return Response(serializer.data)
+## 뉴욕타임즈 ##
 
-
-# 뉴욕타임즈
 def init_NYT_db(request):
     url = f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={settings.NYT_API_KEY}"
     res = requests.get(url)
@@ -63,13 +44,19 @@ class NYTView(APIView):
         return Response(serializer.data)
     
 
+class NYTDetail(APIView):
+    def get(self, request, pk):
+        article = get_object_or_404(NYT, pk=pk)
+        serializer = NYTSerializer(article)
+        return Response(serializer.data)
+
 
 class NYTComment(ListCreateAPIView):
     queryset = NYT_Comment.objects.all()
     serializer_class =NYT_CommentSerializer
 
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         article_id = self.kwargs['pk']
@@ -81,12 +68,36 @@ class NYTComment(ListCreateAPIView):
 
 
 
+## 가디언 ##
+def init_Guardian_db(request):
+    url = f"https://content.guardianapis.com/search?api-key={settings.GUARDIAN_API_KEY}"
+    res = requests.get(url)
+    articles = res.json()['response']['results']
+    for article in articles:
+        try:
+            news_data = Guardian()
+            news_data.title = article['webTitle']
+            news_data.url = article['webUrl']
+            news_data.section = article['sectionName']
+            news_data.save()
+        except:
+            pass
+
+
+
+class GUARDIAN_View(APIView):
+    def get(self, request):
+        articles = Guardian.objects.all()
+        serializer = GuardianSerializer(articles, many=True)
+        return Response(serializer.data)
+
+
 class GuardianComment(ListCreateAPIView):
     queryset = Guardian_Comment.objects.all()
     serializer_class = Guardian_CommentSerializer
 
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         article_id = self.kwargs['pk']
@@ -97,11 +108,7 @@ class GuardianComment(ListCreateAPIView):
         serializer.save(user = user)
 
 
-class NYTDetail(APIView):
-    def get(self, request, pk):
-        article = get_object_or_404(NYT, pk=pk)
-        serializer = NYTSerializer(article)
-        return Response(serializer.data)
+
 
 
 class GuardianDetail(APIView):
