f3f34ef7416ba5a3437f0627478adc5a283f2413
hyobin-yang <hyobin3026@naver.com>
2023-08-10 20:44:18 +0900

    integrate data

--- articles/views.py
+++ articles/views.py
@@ -1,79 +1,74 @@
 from django.shortcuts import render
 import requests
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from .models import Article, Guardian
-from .serializers import ArticleSerializer, GuardianSerializer
+from .models import Article
+from .serializers import ArticleSerializer
 from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-# base_url = 'https://content.guardianapis.com/search'
-# class InitDBView(APIView):
-#     def get(self, request):
-#         url = f'{base_url}?api-key={settings.GUARDIAN_API_KEY}'
-#         res = requests.get(url)
-#         articles = res.json()['response']['results']
-
-#         for article in articles:
-#             news_data = Article()
-#             news_data.title = article['webTitle']
-#             news_data.url = article['webUrl']
-#             # news_data.date = article['webPublicationDate']
-#             news_data.section = article['sectionName']
-#             news_data.save()
-        
-#         articles = Article.objects.all()
-#         serializer = ArticleSerializer(articles, many=True)
-#         return Response(serializer.data)
 
 
-# 가디언
-def init_Guardian_db(request):
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+
+def get_guardian_data(request):
     url = f"https://content.guardianapis.com/search?api-key={settings.GUARDIAN_API_KEY}"
     res = requests.get(url)
     articles = res.json()['response']['results']
-    for article in articles:
-        try:
-            news_data = Guardian()
-            news_data.title = article['webTitle']
-            news_data.url = article['webUrl']
-            news_data.section = article['sectionName']
-            news_data.save()
-        except:
-            pass
+    return articles
 
 
-
-class GUARDIAN_View(APIView):
-    def get(self, request):
-        articles = Guardian.objects.all()
-        serializer = GuardianSerializer(articles, many=True)
-        return Response(serializer.data)
-
-
-# 뉴욕타임즈
-def init_NYT_db(request):
+def get_nyt_data(request):
     url = f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={settings.NYT_API_KEY}"
     res = requests.get(url)
     articles = res.json()['results']
-    for article in articles:
-        try:
-            news_data = Article()
-            news_data.title = article['title']
-            news_data.abstract = article['abstract']
-            news_data.url = article['url']
-            news_data.img_url = article['multimedia'][0]['url']
-            news_data.section = article['section']
-            news_data.paper = 'NYT'
-            news_data.save()
-        except:
-            pass
+    return articles
 
 
-class NYTView(APIView):
-    def get(self, request):
-        articles = Article.objects.filter(paper='NYT')
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+def init_integrate_db(request):
+        guardian_articles = get_guardian_data(request)
+        nyt_articles = get_nyt_data(request)
+
+        for article in nyt_articles:
+            try:
+                news_data = Article()
+                news_data.title = article['title']
+                news_data.abstract = article['abstract']
+                news_data.url = article['url']
+                news_data.img_url = article['multimedia'][0]['url']
+                news_data.section = article['section']
+                news_data.paper = 'NewYorkTimes'
+                news_data.save()
+            except:
+                pass
+
+        for article in guardian_articles:
+            try:
+                news_data = Article()
+                news_data.title = article['webTitle']
+                news_data.url = article['webUrl']
+                news_data.section = article['sectionName']
+                news_data.paper = 'Guardian'
+                news_data.save()
+            except:
+                pass
+
+
+
+
+# class GuardianDetail(APIView):
+#     def get(self, request, pk):
+#         article = get_object_or_404(Guardian, pk=pk)
+#         serializer = GuardianSerializer(article)
+#         return Response(serializer.data)
