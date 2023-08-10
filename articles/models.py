feb803f28c1290674ec493175f3f0b9c34617d40
hyobin-yang <hyobin3026@naver.com>
2023-08-11 00:59:44 +0900

    save

--- articles/models.py
+++ articles/models.py
@@ -2,56 +2,15 @@ from django.db import models
 from accounts.models import User
 
 
-# class NYT(models.Model):
-#     # id = models.AutoField(primary_key=True, null=False, blank=False)
-#     title = models.CharField(max_length=100, unique=True)   # 기사 제목
-#     abstract = models.TextField(default="")    # 기사 요약
-#     url = models.URLField(max_length=1024)     # 기사 URL
-#     img_url = models.URLField(max_length=1024, default="") # 기사 이미지 URL
-#     section = models.CharField(max_length=30)  # 기사 태그
-#     paper = models.CharField(max_length=100, default="")    # 언론사
-
-#     def __str__(self):
-#         return self.title
-    
-
-# class Guardian(models.Model):
-#     id = models.AutoField(primary_key=True, null=False, blank=False)
-#     title = models.CharField(max_length=100, unique=True)   # 기사 제목
-#     url = models.URLField(max_length=1024)     # 기사 URL
-#     section = models.CharField(max_length=30)  # 기사 태그
-#     paper = models.CharField(max_length=100, default="")
-
-#     def __str__(self):
-#         return self.title
-
-
-# class NYT_Comment(models.Model):
-#     post = models.ForeignKey(NYT, null=True, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-#     created_at = models.DateField(auto_now_add=True)
-#     comment = models.TextField()
-
-#     def __str__(self):
-#         return self.comment
-    
-# class Guardian_Comment(models.Model):
-#     post = models.ForeignKey(Guardian, null=True, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-#     created_at = models.DateField(auto_now_add=True)
-#     comment = models.TextField()
-
-#     def __str__(self):
-#         return self.comment
-
 
 class Article(models.Model):
     title = models.CharField(max_length=100, unique=True)
     abstract = models.TextField(default="")
     url = models.URLField(max_length=1024)
-    img_url = models.URLField(max_length=1024, default="")
+    img_url = models.URLField(max_length=1024, default="https://i.guim.co.uk/img/media/29154973f074960f0af69cc6dd7f29cd56de0967/0_50_2016_1210/master/2016.jpg?width=620&dpr=1&s=none")
     section = models.CharField(max_length=30)
     paper = models.CharField(max_length=100)
+    # time = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
--- articles/views.py
+++ articles/views.py
@@ -3,9 +3,7 @@ import requests
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-# from .models import NYT, Guardian, Guardian_Comment, NYT_Comment
 from .models import Article
-# from .serializers import NYTSerializer, GuardianSerializer, NYT_CommentSerializer, Guardian_CommentSerializer
 from .serializers import ArticleSerializer
 from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
@@ -13,13 +11,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from datetime import datetime
+from django.utils import timezone
 
-
+# order_by('-timestamp')
 
 
 class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
+        # articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
@@ -39,126 +40,81 @@ def get_nyt_data(request):
 
 
 def init_integrate_db(request):
-        guardian_articles = get_guardian_data(request)
-        nyt_articles = get_nyt_data(request)
+    guardian_articles = get_guardian_data(request)
+    nyt_articles = get_nyt_data(request)
 
-        for article in nyt_articles:
-            try:
-                news_data = Article()
-                news_data.title = article['title']
-                news_data.abstract = article['abstract']
-                news_data.url = article['url']
-                news_data.img_url = article['multimedia'][0]['url']
-                news_data.section = article['section']
-                news_data.paper = 'NYT'
-                news_data.save()
-            except:
-                pass
+    section_mapping = {
+        'sports': 'Sport',
+        'football': 'Sport',
+        'sport': 'Sport',
+        'nyregion': 'World',
+        'australia-news': 'World',
+        'uk-news': 'World',
+        'us-news': 'World',
+        'us': 'World',
+        'world': 'World',
+        'arts': 'Art',
+        'fashion': 'Art',
+        'movies': 'Film',
+        'theater': 'Film',
+        'film': 'Film',
+        'home': 'Society',
+        'insider': 'Society',
+        'obituaries': 'Society',
+        'opinion': 'Society',
+        'politics': 'Society',
+        'commentisfree': 'Society',
+        'politics': 'Society',
+        'society': 'Society',
+        'environment': 'Society',
+        'news': 'Society',
+        'realestate': 'Society',
+        'sundayreview': 'Society',
+        'upshot': 'Society',
+        'books/review': 'Books',
+        'magazine': 'Books',
+        't-magazine': 'Books',
+        'business': 'Business',
+        'money': 'Business',
+        'automobiles': 'Tech',
+        'science': 'Tech',
+        'technology': 'Tech',
+        'food': 'Culture',
+        'health': 'Culture',
+        'travel': 'Culture',
+        'lifeandstyle': 'Culture',
+        'music': 'Culture',
+        'tv-and-radio': 'Culture',
+    }
+    
+    for article in nyt_articles:
+        try:
+            news_data = Article()
+            news_data.title = article['title']
+            news_data.abstract = article['abstract']
+            news_data.url = article['url']
+            news_data.img_url = article['multimedia'][0]['url']
+            section = article['section']
+            news_data.section = section_mapping.get(section, 'etc')
+            # news_data.time = article['time']
+            news_data.paper = 'NewYorkTimes'
+            news_data.save()
+        except:
+            pass
 
+        
         for article in guardian_articles:
             try:
                 news_data = Article()
                 news_data.title = article['webTitle']
                 news_data.url = article['webUrl']
-                news_data.section = article['sectionName']
+                section = article['sectionName']
+                news_data.section = section_mapping.get(section, 'etc')
+                # news_data.time = article['time']
                 news_data.paper = 'Guardian'
                 news_data.save()
             except:
                 pass
-
-
-
-## 뉴욕타임즈 ##
-
-# def init_NYT_db(request):
-#     url = f"https://api.nytimes.com/svc/topstories/v2/home.json?api-key={settings.NYT_API_KEY}"
-#     res = requests.get(url)
-#     articles = res.json()['results']
-#     for article in articles:
-#         try:
-#             news_data = NYT()
-#             news_data.title = article['title']
-#             news_data.abstract = article['abstract']
-#             news_data.url = article['url']
-#             news_data.img_url = article['multimedia'][0]['url']
-#             news_data.section = article['section']
-#             news_data.paper = 'NYT'
-#             news_data.save()
-#         except:
-#             pass
-
-
-# class NYTView(APIView):
-#     def get(self, request):
-#         articles = NYT.objects.filter(paper='NYT')
-#         serializer = NYTSerializer(articles, many=True)
-#         return Response(serializer.data)
-
-
-# class NYTDetail(APIView):
-#     def get(self, request, pk):
-#         article = get_object_or_404(NYT, pk=pk)
-#         serializer = NYTSerializer(article)
-#         return Response(serializer.data)
-
-
-# class NYTComment(ListCreateAPIView):
-#     queryset = NYT_Comment.objects.all()
-#     serializer_class =NYT_CommentSerializer
-
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAuthenticatedOrReadOnly]
-
-#     def get_queryset(self):
-#         article_id = self.kwargs['pk']
-#         return NYT_Comment.objects.filter(post=article_id)
-
-#     def perform_create(self, serializer):
-#         user = self.request.user
-#         serializer.save(user = user)
-
-
-
-# ## 가디언 ##
-# def init_Guardian_db(request):
-#     url = f"https://content.guardianapis.com/search?api-key={settings.GUARDIAN_API_KEY}"
-#     res = requests.get(url)
-#     articles = res.json()['response']['results']
-#     for article in articles:
-#         try:
-#             news_data = Guardian()
-#             news_data.title = article['webTitle']
-#             news_data.url = article['webUrl']
-#             news_data.section = article['sectionName']
-#             news_data.paper = 'Guardian'
-#             news_data.save()
-#         except:
-#             pass
-
-
-
-# class GUARDIAN_View(APIView):
-#     def get(self, request):
-#         articles = Guardian.objects.all()
-#         serializer = GuardianSerializer(articles, many=True)
-#         return Response(serializer.data)
-
-
-# class GuardianComment(ListCreateAPIView):
-#     queryset = Guardian_Comment.objects.all()
-#     serializer_class = Guardian_CommentSerializer
-
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAuthenticatedOrReadOnly]
-
-#     def get_queryset(self):
-#         article_id = self.kwargs['pk']
-#         return Guardian_Comment.objects.filter(post=article_id)
-
-#     def perform_create(self, serializer):
-#         user = self.request.user
-#         serializer.save(user = user)
-
 
 
 
