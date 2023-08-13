1192734f822ee6f136d10b8ab5f284c3a142759b
Leespect <wewqwew153@gmail.com>
2023-08-13 23:33:14 +0900

    [Feat]: article 공유 횟수 기능

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,24 @@
 from django.db import models
-
+from accounts.models import User
 
 
 class Article(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, unique=True)
     abstract = models.TextField(default="")
     url = models.URLField(max_length=1024)
     img_url = models.URLField(max_length=1024, default="https://i.guim.co.uk/img/media/29154973f074960f0af69cc6dd7f29cd56de0967/0_50_2016_1210/master/2016.jpg?width=620&dpr=1&s=none")
     section = models.CharField(max_length=30)
     paper = models.CharField(max_length=100)
-
+    published_date = models.CharField(max_length=1024, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Article_Like(models.Model):
+    post = models.ForeignKey(Article, null=True, on_delete=models.CASCADE, related_name='article_like')
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='article_like')
+    
+    def __str__(self):
+        return self.user.username
--- community/serializers.py
+++ community/serializers.py
@@ -9,12 +9,14 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'user', 'content', 'created_at', 'like_cnt', 'article']
 
+
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.nickname', read_only=True)
     class Meta:
         model = Comment
         fields = ['id', 'post_id', 'user', 'created_at', 'content', 'like_cnt']
-        
+
+
 class PostDetailSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.nickname', read_only=True)
     comments = CommentSerializer(many=True, read_only=True, source='comment_set')
@@ -22,6 +24,7 @@ class PostDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'user', 'content', 'created_at', 'like_cnt', 'comments', 'article']
+
 
 class PostLikeSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.nickname', read_only=True)
